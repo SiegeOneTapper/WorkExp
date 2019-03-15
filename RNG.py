5d9f90c27277cf70a538153551f7a4b1cd65c58c
SiegeOneTapper <48494803+SiegeOneTapper@users.noreply.github.com>
2019-03-15 13:11:24 +0000

    Deleted old code
    not needed

--- RNG.py
+++
@@ -1,5 +0,0 @@
-#RandomNumberGenerator
-
-print("Systems: sysmain, sysone, systwo, sysshrimp \n Makre sure all system names are entered in lowercase")
-systemChoice = input("Enter name of the system: ")
-noFeed = input("Enter number of feed: 2")
